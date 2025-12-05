82fe4aeda327c0e353ad9f346f0aa78ac80d7241
Mykyta <nikita.borodin1212@gmail.com>
2025-12-05 20:01:19 +0200

    commit

--- app/@modal/(.)notes/[id]/layout.tsx
+++ app/@modal/(.)notes/[id]/layout.tsx
@@ -1,16 +1,5 @@
 import { ReactNode } from "react";
-import NotePreviewClient from "./NotePreview.client";
 
-interface NotesLayoutProps {
-  children: ReactNode;
-  params: { id: string };
-}
-
-export default function NotesLayout({ children, params }: NotesLayoutProps) {
-  return (
-    <>
-      {children}
-      <NotePreviewClient noteId={params.id} />
-    </>
-  );
+export default function NotesLayout({ children }: { children: ReactNode }) {
+  return <>{children}</>;
 }
